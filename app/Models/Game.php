d41e5fd5f074a0b40977157d2b006c8b87ee13b1
ApcarJo <hansokaz@gmail.com>
2021-07-16 10:09:56 +0200

    Added create game controller

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
+
+    protected $fillable = [
+        'gameTitle',
+        'thumbnail_url',
+        'url'
+    ];
+    
     public function party(){
         return $this->hasMany(Party::class);
     }
